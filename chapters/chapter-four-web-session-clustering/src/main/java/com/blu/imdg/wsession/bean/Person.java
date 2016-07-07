b833af5edd9063f2baf677cd67f3ba766cb5527f
shamim <shamimapache@gmail.com>
2016-07-07 17:53:57 +0300

    web session clustering

---
+++ chapters/chapter-four-web-session-clustering/src/main/java/com/blu/imdg/wsession/bean/Person.java
@@ -0,0 +1,50 @@
+package com.blu.imdg.wsession.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by shamim on 07/07/16.
+ */
+public class Person implements Serializable{
+    private String name;
+    private int    age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name.equals(person.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+}
--- chapters/chapter-four-web-session-clustering/src/main/java/com/blu/imdg/wsession/controller/HelloWorldController.java
+++ chapters/chapter-four-web-session-clustering/src/main/java/com/blu/imdg/wsession/controller/HelloWorldController.java
@@ -1,24 +1,33 @@
 package com.blu.imdg.wsession.controller;
 
+import com.blu.imdg.wsession.bean.Person;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
+//@Scope("session")
+@SessionAttributes("personObj")
 public class HelloWorldController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String sayHello(ModelMap model) {
+	public String sayHello(ModelMap model, HttpServletRequest request) {
 		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        //model.addAttribute("personObj", new Person());
+		request.getSession().setAttribute("TEST", new Person("Test"));
 		return "welcome";
 	}
 
-
 	@RequestMapping(value="/helloagain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap model) {
-		model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+	public String sayHelloAgain(ModelMap model, HttpServletRequest request) {
+        // set session
+		model.addAttribute("greeting", request.getSession().getAttribute("TEST"));
 		return "welcome";
 	}
 
